4e13082b74cbbbb00e33ed491bcd2990c3fcba87
t-koberna <trenton_koberna@mines.edu>
2024-05-08 11:42:08 -0600

    Minor Changes
    I made the changes you suggested. Yes that is the convention I am using. I also moved the charge from the participant to the species because that is where it should have been all along

--- spm.py
+++ spm.py
@@ -5,13 +5,13 @@
 # 
 from spm_functions import Half_Cell_Eqlib_Potential,current_density,Species,Participant,Half_Cell
 
-C6 = Species("C",0,0,1)    
-LiC6 = Species("LiC",-230.0,-11.2,1)
-Li_plus = Species("Li+",-293.3,49.7,1)
+C6 = Species("C",0,0,1,0)    
+LiC6 = Species("LiC",-230.0,-11.2,1,0)
+Li_plus = Species("Li+",-293.3,49.7,1,1)
 
-C6_rxn = Participant(C6,0,1,0.5)
-LiC6_rxn = Participant(LiC6,0,1,0.5)
-Li_plus_rxn = Participant(Li_plus,1,1,1)
+C6_rxn = Participant(C6,1,0.5)
+LiC6_rxn = Participant(LiC6,1,0.5)
+Li_plus_rxn = Participant(Li_plus,1,1)
 
 React = [Li_plus_rxn,C6_rxn]
 Prod = [LiC6_rxn]
@@ -19,7 +19,7 @@ Prod = [LiC6_rxn]
 HC = Half_Cell(React,Prod,1,298.15)
 
 U = Half_Cell_Eqlib_Potential(HC)
-print(U)
+print(U,"[V]")
 
 I = current_density(12.3,-0.8,U,HC.Temp)
-print(I)
+print(I,"[mA/cm2]")
