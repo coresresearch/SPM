56802d0f82a48adbf47246b7821d6b964da727a8
t-koberna <trenton_koberna@mines.edu>
2024-05-08 11:11:31 -0600

    Made the changes
    I made the requested changes and imported the function into spm so it now works as intended

--- spm.py
+++ spm.py
@@ -1,4 +1,25 @@
 # spm.py
 #
 # This file serves as the main model file.  
-#   It is called by the user to run the model
+#   It is called by the user to run the mode
+# 
+from spm_functions import Half_Cell_Eqlib_Potential,current_density,Species,Participant,Half_Cell
+
+C6 = Species("C",0,0,1)    
+LiC6 = Species("LiC",-230.0,-11.2,1)
+Li_plus = Species("Li+",-293.3,49.7,1)
+
+C6_rxn = Participant(C6,0,1,0.5)
+LiC6_rxn = Participant(LiC6,0,1,0.5)
+Li_plus_rxn = Participant(Li_plus,1,1,1)
+
+React = [Li_plus_rxn,C6_rxn]
+Prod = [LiC6_rxn]
+
+HC = Half_Cell(React,Prod,1,298.15)
+
+U = Half_Cell_Eqlib_Potential(HC)
+print(U)
+
+I = current_density(12.3,-0.8,U,HC.Temp)
+print(I)
