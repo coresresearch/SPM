7c7266426dfe7a8d9ecfe22030837b2ff513d1ea
t-koberna <trenton_koberna@mines.edu>
2024-06-03 17:13:07 -0600

    After looking it over more, the trends all seem to line up as I am expecting, it is just the scale that is wrong. I suspect the issue is with my initial concentration levels and I also think not having a cathode to pull in Lithium may also be a contributing factor, so I moved on. I attempted to track the potential drop across the seperator and then the behavior of the cathode, but my code is not there yet

--- spm.py
+++ spm.py
@@ -7,11 +7,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from spm_functions import Butler_Volmer as faradaic_current
-from spm_functions import Half_Cell_Eqlib_Potential,residual,Species,Participant,Half_Cell
+from spm_functions import Half_Cell_Eqlib_Potential,residual,Species,Participant,Half_Cell,Electrode
 
 # LiC6 -> C6 + Li+ + e- (reaction at the anode)
 # I do not track the movement of Li through the anode, only the rate of creation of Li+ at the surface
+# The rate Lithium ions enter the electrolyte from the anode is equal to the rate of Li+ enter the seperator and eventually the cathode
+# I assume the concentration in the electrolyte is in uniform so there is no diffusion and there is no bulk movement of the fluid
+#   so there is no convection therefore there is only migration which is driven by the potential difference
+# Potential drop across the electroltye obeys Ohm's Law
+# The cathode and anode have the same composition and dimensions 
 
+# My main question are about how to apply the boundry conditions to make sure that the concentration of Li+ is the same
+# flowing from the anode and into the cathode and the currents are the same throught. My current aproach feels disjointed
 '''
 USER INPUTS
 '''
@@ -28,6 +35,8 @@ C_std = 1000 # Standard Concentration [mol/m^3] (same as 1 M)
 
 i_o = 120 # Exchange Current Density [A/m^2] 
 Cap_dl = 6*10**-3 # Double Layer Capacitance [F/m^2]
+sigma_s = 1.2 # Ionic conductivity for the seperator [1/m-ohm] (this is concentration dependent but it is constant for now)
+t_s =  1e-5 # thickness of the seperator [m]
 T = 298.15 # standard temperature [K]
 n = 1 # number of electrons [mol_electrons/mol_rxn]
 Beta = 0.5 # [-] Beta = (1 - Beta) in this case 
@@ -44,7 +53,8 @@ BnF_RT_a = Beta*n*F/R/T # combine constants for simplicity
 BnF_RT_c = (1-Beta)*n*F/R/T
 
 # Geometry
-V = (4*np.pi/3)*r**3 # Volume of the anode [m^3]
+# Do I need a Volume of the Electroltye, or can I get by with doing everything per unit area?
+Vol_a = (4*np.pi/3)*r**3 # Volume of the anode [m^3]
 A_surf = 4*np.pi*r**2 # geometric surface area of the anode [m^2]
 A_s = 3/r # ratio of surface area to volume for the graphite anode [1/m]
 A_sg = Epsilon_g*Delta_y*A_s # interface surface area per geometric surface area [m^2_interface/m^2_geometric]
@@ -53,13 +63,13 @@ nuA_nF = nu_Li_plus*A_s/n/F # combine constants for convenience
 
 # Initial Values
 X_g_0 = 1 - X_Li_0 # Initial Mole Fraction of Graphite [-]
-N_g = V*rho_g/MW_g # The number of moles of graphite in the Anode [mol] (this number does not change)
-C_g = N_g/V # Concentration of graphite in the anode [mol/m^3] (this number does not change)
+N_g = Vol_a*rho_g/MW_g # The number of moles of graphite in the Anode [mol] (this number does not change)
+C_g = N_g/Vol_a# Concentration of graphite in the anode [mol/m^3] (this number does not change)
 N_Li_0 = N_g*(1/X_g_0 - 1) # Initial number of Moles of Lithium in the Anode [mol]
-C_Li_0 = N_Li_0/V # initial value for Lithium in the Anode [mol_Li/m^3]
+C_Li_0 = N_Li_0/Vol_a# initial value for Lithium in the Anode [mol_Li/m^3]
 
 '''
-Set up the Half Cell
+Set up the Half Cells
 '''
 C6 = Species("C",0,0,C_std,0)    
 LiC6 = Species("LiC6",-230.0,-11.2,C_std,0)
@@ -72,54 +82,68 @@ Li_plus_rxn = Participant(Li_plus,1,C_Li_plus)
 React = [LiC6_rxn]
 Prod = [Li_plus_rxn,C6_rxn]
 
-HC = Half_Cell(React,Prod,n,T) # Create the Half Cell object
-indx_Li = 1 # index for solid lithium in the anode in the half cell object
+HC_a = Half_Cell(React,Prod,n,T) # Create the Half Cell object
+indx_Li_a = 1 # index for solid lithium in the anode in the half cell object
+
+HC_c = Half_Cell(Prod,React,n,T) # Create the Half Cell object
+indx_Li_c = 0 # index for solid lithium in the cathode in the half cell object
+
+'''
+Set up Electrodes
+'''
+# For now all that is different is the half cell reation directions
+Anode = Electrode(BnF_RT_a,BnF_RT_c,Cap_dl,i_o,A_sg,nuA_nF,HC_a,indx_Li_a)
+Cathode = Electrode(BnF_RT_a,BnF_RT_c,Cap_dl,i_o,A_sg,nuA_nF,HC_c,indx_Li_c)
 
 '''
 Integration
 '''
 t_start = 0 # [s]
-t_end = 1e-2 # length of time passed in the integration [s]
+t_end = 1e-5 # length of time passed in the integration [s]
 t_span = [t_start,t_end]
-SV_0 = [Phi_dl_0,C_Li_0] # initial values
-N = 10000 # number of time steps
+SV_0 = [Phi_dl_0,C_Li_0] #,-Phi_dl_0,C_Li_0] # initial values
+N = 1000 # number of time steps
 
-SV = solve_ivp(residual,t_span,SV_0,method='BDF',args=(i_ext,BnF_RT_a,BnF_RT_c,Cap_dl,i_o,A_sg,nuA_nF,HC,indx_Li),
+SV = solve_ivp(residual,t_span,SV_0,method='BDF',args=(i_ext,Anode,Cathode,sigma_s,t_s),
                rtol = 1e-5,atol = 1e-8)
 
 '''
 Post Processing
 '''
-U_cell = np.zeros_like(SV.t)
-i_fa = np.zeros_like(SV.t)
+## Anode
+U_cell_a = np.zeros_like(SV.t)
+i_far_a = np.zeros_like(SV.t)
 for ind, ele in enumerate(SV.t):
-    HC.C[1] = SV.y[1,ind]
-    U_cell[ind] = Half_Cell_Eqlib_Potential(HC) # Open Cell Potential [V]
+    HC_a.C[1] = SV.y[1,ind]
+    U_cell_a[ind] = Half_Cell_Eqlib_Potential(HC_a) # Open Cell Potential [V]
     
-    i_fa[ind] = faradaic_current(i_o,SV.y[0,ind],U_cell[ind],BnF_RT_a,BnF_RT_c) # Faradaic Current [A/m^2]
+    i_far_a[ind] = faradaic_current(i_o,SV.y[0,ind],U_cell_a[ind],BnF_RT_a,BnF_RT_c) # Faradaic Current [A/m^2]
 
-i_dl = i_ext/A_sg - i_fa # Double Layer current [A/m^2]
+i_dl_a = i_ext/A_sg - i_far_a # Double Layer current [A/m^2]
 i_ex = i_ext/A_sg*np.ones_like(SV.t) # External Layer current [A/m^2]
 
 # Double Layer Potential Difference 
 plt1 = plt.figure(1)
 plt.plot(SV.t,SV.y[0])
 plt.ylim((0,2))
+plt.title(r"$\Delta\Phi_{dl}$ for the Anode")
 plt.xlabel("time [s]")
 plt.ylabel("Change in Potential Across the Double Layer [V]")
 plt1.show()
 
 # Current
 plt2 = plt.figure(2)
-plt.plot(SV.t,i_dl,'.-')
+plt.plot(SV.t,i_dl_a,'.-')
 plt.plot(SV.t,i_ex)
-plt.plot(SV.t,i_fa)
-plt.ylim((-1.5*i_ext/A_sg ,1.5*i_ext/A_sg))
+plt.plot(SV.t,i_far_a)
+plt.ylim((-1.5*i_ext/A_sg ,2*i_ext/A_sg))
+plt.title("Current in the Anode")
 plt.xlabel("time [s]")
 plt.ylabel("Current [A]")
-plt.legend(['DL','EXT','FAR'])
+plt.legend(['DL','EXT','FAR'], loc = 'lower right')
 plt2.show()
 
+# The trends for these two plots are correct, but the scale is wrong 
 # Concentration
 plt3 = plt.figure(3)
 plt.plot(SV.t,SV.y[1])
@@ -129,5 +153,8 @@ plt3.show()
 
 # Open Cell Potential
 plt4 = plt.figure(4)
-plt.plot(SV.t,U_cell)
+plt.plot(SV.t,U_cell_a)
+plt.title("Open Cell Potential for the Anode")
+plt.xlabel("time [s]")
+plt.ylabel("U [V]")
 plt.show()
