f4cc8265cce02b316e7388c380cfcbc3eaed3ec6
t-koberna <trenton_koberna@mines.edu>
2024-06-11 15:35:46 -0600

    There was not much to track with the 'n's but I did go through and comment where it came up. I also made it so both reaction proceed in the same direction and I better organized the plots

--- spm.py
+++ spm.py
@@ -7,73 +7,88 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from spm_functions import Butler_Volmer as faradaic_current
-from spm_functions import Half_Cell_Eqlib_Potential,residual,Species,Participant,Half_Cell,Electrode
+from spm_functions import Half_Cell_Eqlib_Potential, residual, Species, Participant, Half_Cell
 
-# LiC6 -> C6 + Li+ + e- (reaction at the anode)
+# LiC6 -> Li+ + C6 + e- (reaction at the anode and cathode)
 # I do not track the movement of Li through the anode, only the rate of creation of Li+ at the surface
 # The rate Lithium ions enter the electrolyte from the anode is equal to the rate of Li+ enter the seperator and eventually the cathode
 # I assume the concentration in the electrolyte is in uniform so there is no diffusion and there is no bulk movement of the fluid
 #   so there is no convection therefore there is only migration which is driven by the potential difference
 # Potential drop across the electroltye obeys Ohm's Law
 # The cathode and anode have the same composition and dimensions 
+# I do not create an instance of the species/particiant classes for the electron so I track the 
+#   sign of the electron speperately using 'n'
 
-# My main question are about how to apply the boundry conditions to make sure that the concentration of Li+ is the same
-# flowing from the anode and into the cathode and the currents are the same throught. My current aproach feels disjointed
+# The cell potential is twice 
+
+plot_Cathode = True
+plot_Anode = True
 '''
 USER INPUTS
 '''
 # 
+# Operating Conditions
 i_ext = -2000 # external current [A/m^2]
-Phi_dl_0 = 1.5 #-U+ 0.75 # initial value for Phi_dl [V]
+T = 298.15 # standard temperature [K]
 
-nu_Li_plus = 1 # stoichiometric coefficient of Lithium [mol_Li+/mol_rxn]
+# Initial Conditions
+Phi_dl_0 = 1.5 #-U+ 0.75 # initial value for Phi_dl [V]
 X_Li_0 = 0.5 # Initial Mole Fraction of the Anode for Lithium [-]
+
+# Material parameters:
 MW_g = 12 # Molectular Weight of Graphite [g/mol]
 rho_g = 2.2e6 # Denstity of graphite [g/m^3]
-C_Li_plus = 1 # Concentration of Li+ in the Electrolyte [mol/L] (I assume electrolyte transport is fast)
+C_Li_plus = 1000 # Concentration of Li+ in the Electrolyte [mol/m^2] (I assume electrolyte transport is fast)
 C_std = 1000 # Standard Concentration [mol/m^3] (same as 1 M)
-
-i_o = 120 # Exchange Current Density [A/m^2] 
-Cap_dl = 6*10**-3 # Double Layer Capacitance [F/m^2]
 sigma_s = 1.2 # Ionic conductivity for the seperator [1/m-ohm] (this is concentration dependent but it is constant for now)
-t_s =  1e-5 # thickness of the seperator [m]
-T = 298.15 # standard temperature [K]
-n = 1 # number of electrons [mol_electrons/mol_rxn]
+
+# Kinetic parameters
+i_o = 120 # Exchange Current Density [A/m^2] 
+Cap_dl = 6*10**-5 # Double Layer Capacitance [F/m^2]
 Beta = 0.5 # [-] Beta = (1 - Beta) in this case 
+# both Li+ and electrons are products in this reaction so they have postive coefficients
+nu_Li_plus = 1 # stoichiometric coefficient of Lithium [mol_Li+/mol_rxn]
+n = 1 # number of electrons [mol_electrons/mol_rxn]
+
+
+# Microstructure
+t_s =  1e-5 # thickness of the seperator [m]
 Delta_y = 50*10**-6 # Anode thickness [m]
 r = 5*10**-6 # particle radius [m]
 Epsilon_g = 0.65 # volume fraction fo graphite [-]
+
+# Simulation parameters
+dPhi_min = 0.2 # Minimum voltage at which to terminate the integration [V]
 
 '''
 Constants and Parameters
 ''' 
 F = 96485.34 #Faraday's number [C/mol_electron]
 R = 8.3145 #Universal gas constant [J/mol-K]
-BnF_RT_a = Beta*n*F/R/T # combine constants for simplicity
-BnF_RT_c = (1-Beta)*n*F/R/T
 
 # Geometry
-# Do I need a Volume of the Electroltye, or can I get by with doing everything per unit area?
-Vol_a = (4*np.pi/3)*r**3 # Volume of the anode [m^3]
-A_surf = 4*np.pi*r**2 # geometric surface area of the anode [m^2]
+Vol_a = (4*np.pi/3)*r**3 # Volume of a single anode particle [m^3]
+A_surf = 4*np.pi*r**2 # geometric surface area of a single anode particle [m^2]
 A_s = 3/r # ratio of surface area to volume for the graphite anode [1/m]
 A_sg = Epsilon_g*Delta_y*A_s # interface surface area per geometric surface area [m^2_interface/m^2_geometric]
 
-nuA_nF = nu_Li_plus*A_s/n/F # combine constants for convenience
-
 # Initial Values
 X_g_0 = 1 - X_Li_0 # Initial Mole Fraction of Graphite [-]
-N_g = Vol_a*rho_g/MW_g # The number of moles of graphite in the Anode [mol] (this number does not change)
-C_g = N_g/Vol_a# Concentration of graphite in the anode [mol/m^3] (this number does not change)
-N_Li_0 = N_g*(1/X_g_0 - 1) # Initial number of Moles of Lithium in the Anode [mol]
-C_Li_0 = N_Li_0/Vol_a# initial value for Lithium in the Anode [mol_Li/m^3]
+# The number of moles of C6 in a single anode particle [mol]. Divide by 6 to 
+#    convert MW from per mole of carbon to per mole of C6 [mol]
+N_g = Vol_a*rho_g/MW_g/6
+# Molar concentration of C6 in the anode [mol/m^3]
+C_g = N_g/Vol_a
+# Initial Lithium molar concentration in the Anode [mol_Li/m^3-graphite]
+C_Li_0 = C_g*X_Li_0 
 
 '''
 Set up the Half Cells
 '''
-C6 = Species("C",0,0,C_std,0)    
-LiC6 = Species("LiC6",-230.0,-11.2,C_std,0)
-Li_plus = Species("Li+",-293.3,49.7,C_std,1)
+# Reacation: LiC6 -> C6 + Li+ + e-
+C6 = Species("C",0,0,C_g,0)
+LiC6 = Species("LiC6",-230000,-11.2,C_g,0)
+Li_plus = Species("Li+",-293300,49.7,C_std,1)
 
 C6_rxn = Participant(C6,1,C_g)
 LiC6_rxn = Participant(LiC6,1,C_Li_0)
@@ -82,79 +97,131 @@ Li_plus_rxn = Participant(Li_plus,1,C_Li_plus)
 React = [LiC6_rxn]
 Prod = [Li_plus_rxn,C6_rxn]
 
-HC_a = Half_Cell(React,Prod,n,T) # Create the Half Cell object
-indx_Li_a = 1 # index for solid lithium in the anode in the half cell object
-
-HC_c = Half_Cell(Prod,React,n,T) # Create the Half Cell object
-indx_Li_c = 0 # index for solid lithium in the cathode in the half cell object
-
-'''
-Set up Electrodes
-'''
-# For now all that is different is the half cell reation directions
-Anode = Electrode(BnF_RT_a,BnF_RT_c,Cap_dl,i_o,A_sg,nuA_nF,HC_a,indx_Li_a)
-Cathode = Electrode(BnF_RT_a,BnF_RT_c,Cap_dl,i_o,A_sg,nuA_nF,HC_c,indx_Li_c)
+# Both electrodes have the same reation and in this forward reaction one electron is 
+#   produced so n = 1 (as it is defined above in the inputs)
+Anode = Half_Cell(React,Prod,n,T,Beta,F,R,Cap_dl,i_o,A_sg,A_s,'LiC6','Li+')
+Cathode = Half_Cell(React,Prod,n,T,Beta,F,R,Cap_dl,i_o,A_sg,A_s,'LiC6','Li+')
 
 '''
 Integration
 '''
 t_start = 0 # [s]
-t_end = 1e-5 # length of time passed in the integration [s]
+t_end = 3e1 # length of time passed in the integration [s]
 t_span = [t_start,t_end]
-SV_0 = [Phi_dl_0,C_Li_0] #,-Phi_dl_0,C_Li_0] # initial values
-N = 1000 # number of time steps
+SV_0 = [Phi_dl_0,C_Li_0,-Phi_dl_0,C_Li_0] # initial values
 
-SV = solve_ivp(residual,t_span,SV_0,method='BDF',args=(i_ext,Anode,Cathode,sigma_s,t_s),
-               rtol = 1e-5,atol = 1e-8)
+
+def min_voltage(_,SV,i_ext,Anode,Cathode):
+    return SV[0] - dPhi_min
+
+min_voltage.terminal = True
+
+SV = solve_ivp(residual,t_span,SV_0,method='BDF',
+               args=(i_ext,Anode,Cathode),
+               rtol = 1e-5,atol = 1e-8, events=min_voltage)
 
 '''
 Post Processing
 '''
-## Anode
+## Anode LiC6 -> Li+ + C6 + e- 
 U_cell_a = np.zeros_like(SV.t)
 i_far_a = np.zeros_like(SV.t)
 for ind, ele in enumerate(SV.t):
-    HC_a.C[1] = SV.y[1,ind]
-    U_cell_a[ind] = Half_Cell_Eqlib_Potential(HC_a) # Open Cell Potential [V]
+    # I find open cell potential and faradic current using the same process as the residual function
+    Anode.C[0] = SV.y[1,ind]/Anode.C_int[0]
+    Anode.C[-1] = 1. - Anode.C[0]
+    U_cell_a[ind] = Half_Cell_Eqlib_Potential(Anode) # Open Cell Potential [V]
     
-    i_far_a[ind] = faradaic_current(i_o,SV.y[0,ind],U_cell_a[ind],BnF_RT_a,BnF_RT_c) # Faradaic Current [A/m^2]
+    i_far_a[ind] = faradaic_current(i_o,SV.y[0,ind],U_cell_a[ind],Anode.BnF_RT_a,Anode.BnF_RT_c) # Faradaic Current [A/m^2]
 
 i_dl_a = i_ext/A_sg - i_far_a # Double Layer current [A/m^2]
-i_ex = i_ext/A_sg*np.ones_like(SV.t) # External Layer current [A/m^2]
 
+## Cathode LiC6 -> Li+ + C6 + e- 
+U_cell_c = np.zeros_like(SV.t)
+i_far_c = np.zeros_like(SV.t)
+for ind, ele in enumerate(SV.t):
+    Cathode.C[0] = SV.y[3,ind]/Cathode.C_int[0]
+    Cathode.C[-1] = 1. - Cathode.C[0]
+    U_cell_c[ind] = Half_Cell_Eqlib_Potential(Cathode) # Open Cell Potential [V]   
+    
+    i_far_c[ind] = faradaic_current(i_o,SV.y[2,ind],U_cell_c[ind],Cathode.BnF_RT_a,Cathode.BnF_RT_c) # Faradaic Current [A/m^2]
+i_dl_c = i_ext/A_sg + i_far_c # Double Layer current [A/m^2]
+
+## Seperator
+i_ex = i_ext/A_sg*np.ones_like(SV.t) # External current as an array [A/m^2]
+Delta_Phi_s = -i_ex*t_s/sigma_s # Potential drop across the seperator [V]
+
+## Whole Cell
+# Phi_c - Phi_el_c = delta_Phi_dl_c  
+# Phi_a - Phi_el_a = delta_Phi_dl_a ; 
+
+# V_cell = Phi_c - Phi_a 
+# V_cell = Ph_c - Phi_elc_c - Phi_elc_a - Phi_a
+
+# V_cell = delta_Phi_dl_c + delta_Phi_dl_a
+V_cell = SV.y[2] + SV.y[0]
+
+'''
+Plotting
+'''
 # Double Layer Potential Difference 
 plt1 = plt.figure(1)
 plt.plot(SV.t,SV.y[0])
-#plt.ylim((0,2))
-plt.title(r"$\Delta\Phi_{dl}$ for the Anode")
+plt.plot(SV.t,SV.y[2])
+plt.title(r"$\Delta\Phi_{dl}$")
 plt.xlabel("time [s]")
 plt.ylabel("Change in Potential Across the Double Layer [V]")
-plt1.show()
+plt.legend(['Anode','Cathode'], loc = 'upper right')
 
-# Current
+# Current to the Anode
 plt2 = plt.figure(2)
 plt.plot(SV.t,i_dl_a,'.-')
-plt.plot(SV.t,i_ex)
-plt.plot(SV.t,i_far_a)
+plt.plot(SV.t, i_ex, linewidth=4)
+plt.plot(SV.t,i_far_a, color='black')
 plt.ylim((-2*abs(i_ext)/A_sg ,1.5*abs(i_ext)/A_sg))
 plt.title("Current in the Anode")
 plt.xlabel("time [s]")
 plt.ylabel("Current [A]")
 plt.legend(['DL','EXT','FAR'], loc = 'lower right')
-plt2.show()
 
-# The trends for these two plots are correct, but the scale is wrong 
-# Concentration
+# Current to the Cathode
 plt3 = plt.figure(3)
-plt.plot(SV.t,SV.y[1])
+plt.plot(SV.t,i_dl_c,'.-')
+plt.plot(SV.t, i_ex, linewidth=4)
+plt.plot(SV.t,i_far_c, color='black')
+plt.ylim((-2*abs(i_ext)/A_sg ,1.5*abs(i_ext)/A_sg))
+plt.title("Current in the Cathode")
 plt.xlabel("time [s]")
-plt.ylabel("Concentration of Li in the Anode [mol/m^3]")
-plt3.show()
+plt.ylabel("Current [A]")
+plt.legend(['DL','EXT','FAR'], loc = 'lower right')
+
+# Concentration
+plt4 = plt.figure(4)
+plt.plot(SV.t,SV.y[1])
+plt.plot(SV.t,SV.y[3])
+plt.xlabel("time [s]")
+plt.ylabel("Concentration of Li [mol/m^3]")
+plt.legend(['Anode','Cathode'], loc = 'upper left')
 
 # Open Cell Potential
-plt4 = plt.figure(4)
+plt5 = plt.figure(5)
 plt.plot(SV.t,U_cell_a)
-plt.title("Open Cell Potential for the Anode")
+plt.plot(SV.t,U_cell_c)
+plt.title("Open Cell Potential")
 plt.xlabel("time [s]")
-plt.ylabel("U [V]")
+plt.ylabel("Equilibrium Potential [V]")
+plt.legend(['Anode','Cathode'], loc = 'upper left')
+
+# Cell Voltage
+plt6 = plt.figure(6)
+plt.plot(SV.t,SV.y[0],'--')
+plt.plot(SV.t,SV.y[2],'--')
+plt.plot(SV.t,Delta_Phi_s,'--')
+plt.plot(SV.t,V_cell)
+plt.legend([r'$DL_A$',r'$DL_C$','Sep', r'$V_{cell}$'], loc = 'best')
+plt.title("Potential Difference Between the Electrodes")
+plt.xlabel("time [s]")
+plt.ylabel("Voltage [V]")
+
 plt.show()
+
--- spm_functions.py
+++ spm_functions.py
@@ -34,6 +34,7 @@ def Half_Cell_Eqlib_Potential(HalfCell,F = 96485.34, T_amb = 298.15, R = 8.3145)
     U_Cell = U_0_Cell - R*T/n_elc/F*np.log(np.prod(np.power(HalfCell.C,HalfCell.nu))) # adjust for concentration
     return U_Cell
 
+
 def Butler_Volmer(i_o,V,U,BnF_RT_a,BnF_RT_c):
     """
     This function calculates the faraday current density at the electrode-electrolyte interface, using the
@@ -70,9 +71,9 @@ class Species:
     """
     def __init__(self, Name, Gibbs_energy_formation, Standard_Entropy,Standard_State,charge):
         self.name = Name
-        self.DG_f = Gibbs_energy_formation # [kJ/mol]
+        self.DG_f = Gibbs_energy_formation # [J/mol]
         self.S = Standard_Entropy          # [J/mol-K]
-        self.state = Standard_State        # [mol/m^3]
+        self.C_int = Standard_State          # [mol/m^3]
         self.charge = charge               # [elementary charge]
 
 class Participant(Species):
@@ -81,8 +82,8 @@ class Participant(Species):
     Takes in the Species, stoichiometric coefficient, and concentration
     """
     def __init__(self, Species, stoichiometric_coefficient, concentration):
-        super().__init__(Species.name, Species.DG_f, Species.S, Species.state,Species.charge)
-        self.stoich_coeff = stoichiometric_coefficient
+        super().__init__(Species.name, Species.DG_f, Species.S, Species.C_int ,Species.charge)
+        self.stoich_coeff = stoichiometric_coefficient # Takes in the magnitude, the sign is added when creating a Half Cell
         self.C = concentration             # [mol/m^3]
 
 class Half_Cell:
@@ -90,21 +91,28 @@ class Half_Cell:
     Takes in the reactants, products, number of electrons per mol of reaction, and the temerature of the half cell in Kelvin
     Stores each property in an array where the reactants are followed by the products
     """
-    def __init__(self, Reactants,Products,n,Temperature):
+    def __init__(self,Reactants,Products,n,Temperature,Beta,F,R,Cap,i_o,A_sg,A_s,Species_Tracked,ion):
         self.n = n
         self.Temp = Temperature
+        self.BnF_RT_a = Beta*n*F/R/Temperature
+        self.BnF_RT_c = (1-Beta)*n*F/R/Temperature
+        self.Cap = Cap
+        self.i_o = i_o
+        self.A = A_sg
         indx = 0
         self.name = [None]*(len(Reactants)+len(Products))
         self.G = [None]*(len(Reactants)+len(Products))
         self.S = [None]*(len(Reactants)+len(Products))
         self.C = [None]*(len(Reactants)+len(Products))
         self.nu = [None]*(len(Reactants)+len(Products))
+        self.C_int = [None]*(len(Reactants)+len(Products))
         for i in Reactants:
             self.name[indx] = i.name
             self.G[indx] = i.DG_f
             self.S[indx] = i.S
             self.C[indx] = i.C
             self.nu[indx] = i.stoich_coeff*-1
+            self.C_int[indx] = i.C_int
             indx = indx + 1
         for i in Products:
             self.name[indx] = i.name
@@ -112,69 +120,71 @@ class Half_Cell:
             self.S[indx] = i.S
             self.C[indx] = i.C
             self.nu[indx] = i.stoich_coeff
+            self.C_int[indx] = i.C_int
             indx = indx + 1
+        self.ind_track = self.name.index(Species_Tracked)
+        # I no longer force this to be postive. The coeff of th ion is automatically set, but the 
+        #   value for n if determed by the user since I do not include the elctron as a species in the reaction
+        self.nuA_nF = self.nu[self.name.index(ion)]*A_s/n/F
 
-def residual(_,SV,i_ext,Anode,Cathode,sigma_s,t_s):
+def residual(_,SV,i_ext,Anode,Cathode):
     '''
     Derivations (a=anode,s=sperator,c=cathode)
 
     Change in Double Layer potential Anode [0]:
-    Eta_a = Phi_a - Phi_el_a - U_a ; Phi_an = 0 ; Phi_a = Phi_el_a - delta_Phi_dl_a
-    Eta_a = - delta_Phi_dl_a - U_a => sub into Butler Volmer
+    Eta_a = Phi_a - Phi_el_a - U_a ; Phi_an = 0 ; Phi_a - Phi_el_a = delta_Phi_dl_a
+    Eta_a = delta_Phi_dl_a - U_a => sub into Butler Volmer
     i_ext/A_sg = i_dl_a + i_far_a ; -i_dl_a = Cap_dl_a*(d Delta_Phi_dl_a/ dt) 
     (i_far_a - i_ext/A_sg)/Cap_dl_a = d Delta_Phi_dl_a/dt
 
     Change in Lithium concentration in the Anode [1]:
-    dN_Li/dt = -s_dot_Li+*A_surf*N_p ; dC_Li/dt = (dN_Li/dt)/(V*N_p) ; s_dot_Li+ = -i_far*nu_Li+/(n*F) ; A_suf/V = A_s
+    dN_Li/dt = -s_dot_Li+*A_surf*N_p ; dC_Li/dt = (dN_Li/dt)/(Vol*N_p) ; s_dot_Li+ = -i_far*nu_Li+/(n*F) ; A_suf/Vol = A_s
     dC_Li/dt = i_far*nu_Li+*A_s/(n*F)
     
     Change in Double Layer potential Cathode [2]: 
-    Eta_c = Phi_c - Phi_el_c - U_c ; Delta_Phi_dl_c = Phi_c - Phi_el_c 
-    Eta_c = Delta_Phi_dl_c - U_c => sub into Butler Volmer
-    i_ext/A_sg = i_dl_c + i_far_c ; i_dl_c = Cap_dl_c*(d Delta_Phi_c_dl/ dt) 
-    (i_far_c - i_ext/A_sg)/Cap_dl_c = d Delta_Phi_c_dl/dt
+    Eta_c = Phi_c - Phi_el_c - U_c ; Phi_c - Phi_el_c = delta_Phi_dl_c 
+    Eta_c = delta_Phi_dl_a - U_c => sub into Butler Volmer
+    -i_ext/A_sg = i_dl_c + i_far_c ; -i_dl_c = Cap_dl_c*(d Delta_Phi_c_dl/ dt) 
+    (i_far_c + i_ext/A_sg)/Cap_dl_c = d Delta_Phi_c_dl/dt
+    
+    Change in Lithium concentration in the Cathode [3]:
+    dC_Li/dt = i_far*nu_Li+*A_s/(n*F)
     '''
+    # The convention for double layer potential is electrode minus electrolyte
+    # For this reaction, Li+ and the electron are both on the same side of the reaction,
+    #   so the signs of their coefficients will always be the same which means nuA_nF
+    #   is positive. As a result, a positive i_far will cause the concentration
+    #   of lithium in the electrode to increase
+    # I hard code in the positions for C6 in this reaction. Since I use the reaction
+    #   LiC6 -> Li+ + C6 + e- for both half cells, grahite is the last partcipant in 
+    #   the list in both HCs
+     
     # Anode
     V_a = SV[0]
-    C_Li = SV[Anode.indx_Li]
-    Anode.HC.C[Anode.indx_Li] = C_Li # Updates the concentraion of Lithium in the anode
     
-    U_a = Half_Cell_Eqlib_Potential(Anode.HC)
+    C_Li_a = SV[1]/Anode.C_int[Anode.ind_track]
+    Anode.C[Anode.ind_track] = C_Li_a
+    Anode.C[-1] = 1. - C_Li_a
+    
+    U_a = Half_Cell_Eqlib_Potential(Anode)
     i_far_a= Butler_Volmer(Anode.i_o,V_a,U_a,Anode.BnF_RT_a,Anode.BnF_RT_c)
    
     dPhi_dl_a_dt = (i_far_a - i_ext/Anode.A)/Anode.Cap  # returns an expression for d Delta_Phi_dl/dt in terms of Delta_Phi_dl
     dC_Li_a_dt = i_far_a*Anode.nuA_nF # returns an expression for dC_Li/dt in terms of Delta_Phi_dl
     
-    # Seperator
-    # I am not sure if I should set the current in the sperator based off of the change in concentraion of Li+, 
-    # or if I should set it equal to the faradaic current leaving the anode, or if it is i_ext
-    i_sep = dC_Li_a_dt*Anode.nuA_nF/Anode.A*t_s # using this as a round about way to avoid passing in F
-    Delta_Phi_s = -i_sep*t_s/sigma_s # Potential drop across the seperator [V]
-    
-    # I get a math range error in this part which is why I have it commented out for now
-    '''
     # Cathode
-    V_c = Delta_Phi_c + SV[2]
-    U_c = Half_Cell_Eqlib_Potential(Cathode.HC)
-    i_far_c= Butler_Volmer(Cathode.i_o,V_c,U_c,Cathode.BnF_RT_a,Cathode.BnF_RT_c)
+    V_c = SV[2]
     
-    dPhi_dl_c_dt = (i_far_c - i_ext/Cathode.A)/Cathode.Cap
-    dC_Li_c_dt = i_far_c*Cathode.nuA_nF 
-    '''
-    dSVdt = [dPhi_dl_a_dt,dC_Li_a_dt] #,dPhi_dl_c_dt,dC_Li_c_dt]
+    C_Li_c = SV[3]/Cathode.C_int[Cathode.ind_track]
+    Cathode.C[Cathode.ind_track] = C_Li_c
+    Cathode.C[-1] = 1. - C_Li_c
+    
+    U_c = Half_Cell_Eqlib_Potential(Cathode)
+    i_far_c = Butler_Volmer(Cathode.i_o,V_c,U_c,Cathode.BnF_RT_a,Cathode.BnF_RT_c)
+    
+    dPhi_dl_c_dt = (i_far_c + i_ext/Cathode.A)/Cathode.Cap
+    dC_Li_c_dt = i_far_c*Cathode.nuA_nF
+
+    dSVdt = [dPhi_dl_a_dt, dC_Li_a_dt, dPhi_dl_c_dt, dC_Li_c_dt]
     
     return dSVdt
-
-class Electrode:
-    '''
-    Holds all of the parameters of an electrode needed in the residual function
-    '''
-    def __init__(self, BnF_RT_a,BnF_RT_c,Cap,i_o,A,nuA_nF,HC,indx_Li):
-        self.BnF_RT_a = BnF_RT_a
-        self.BnF_RT_c = BnF_RT_c
-        self.Cap = Cap
-        self.i_o = i_o
-        self.A = A
-        self.nuA_nF = nuA_nF
-        self.HC = HC
-        self.indx_Li = indx_Li
